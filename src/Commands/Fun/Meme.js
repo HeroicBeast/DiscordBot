00eeb6234b2981cd78ba639e818e6f1f4d1eb6b7
HeroicBeast <73134851+HeroicBeast@users.noreply.github.com>
2021-05-31 20:06:49 +0530

    made a mistake in v.redd.it

--- src/Commands/Fun/Meme.js
+++ src/Commands/Fun/Meme.js
@@ -53,13 +53,13 @@ const run = async (client, message, args) => {
 	];
 	const subreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
 	const meme = await api.advanced(subreddit, 'top');
-	if (meme.img.includes('imgur'))
+	if (meme.img.toLowerCase().includes('imgur'))
 		return message.channel.send(`Couldn't Get That Meme.. Try Again?`);
-	if (meme.img.includes('gallery'))
+	if (meme.img.toLowerCase().includes('gallery'))
 		return message.channel.send(`Couldn't Get That Meme.. Try Again?`);
-	if (meme.img.includes('v.reddit'))
+	if (meme.img.toLowerCase().includes('v.redd.it'))
 		return message.channel.send(`Couldn't Get That Meme.. Try Again?`);
-	if (meme.img.includes('youtu.be'))
+	if (meme.img.toLowerCase().includes('youtu.be'))
 		return message.channel.send(`Couldn't Get That Meme.. Try Again?`);
 	message.channel.send(
 		client
