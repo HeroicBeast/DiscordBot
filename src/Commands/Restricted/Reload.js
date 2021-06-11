4cfdd9a3ad48166a90d59ba2e895825a703edfb2
HeroicBeast <73134851+HeroicBeast@users.noreply.github.com>
2021-06-11 08:36:16 +0530

    Update Reload.js

--- src/Commands/Restricted/Reload.js
+++ src/Commands/Restricted/Reload.js
@@ -10,7 +10,7 @@ const glob_1 = __importDefault(require('glob'));
 const run = async (client, message, args) => {
 	if (
 		message.author.id !== '656432172722290688' &&
-		message.author.id !== '648031359096586247'&& 
+		message.author.id !== '648031359096586247' && 
   		message.author.id !== '836080543249596426
 	)
 		return message.channel.send('Dev Only Command');
