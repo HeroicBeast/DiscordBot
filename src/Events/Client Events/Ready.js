ff71b39c6e92f6433aa9abe4654d94ce27d0a696
HeroicBeast <73134851+HeroicBeast@users.noreply.github.com>
2021-06-11 23:48:26 +0530

    Updating Ready.js

--- src/Events/Client Events/Ready.js
+++ src/Events/Client Events/Ready.js
@@ -2,13 +2,12 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.name = exports.run = void 0;
 const run = async (client) => {
-	client.logger.success(
-		`${client.user.tag} is Online!\n${client.guilds.cache.size} Servers!\n${
-			client.users.cache.size - 1
-		} Users!`
-	);
 let count = 0;
 client.guilds.cache.forEach(guild => count += guild.memberCount)
+	client.logger.success(
+		`${client.user.tag} is Online!\n${client.guilds.cache.size} Servers!\n${count} Users!`
+	);
+
 	client.user.setActivity(
 		`${client.guilds.cache.size} Servers and ${count} Users! || Invite Me At dsc.gg/thenexusbot`,
 		{ type: 'WATCHING' }
