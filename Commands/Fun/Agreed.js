d338ef9b6d7210b26c6e66c83be56a62ae01fa00
HeroicBeast <73134851+HeroicBeast@users.noreply.github.com>
2022-01-14 12:44:33 +0530

    user argument

--- Commands/Fun/Agreed.js
+++ Commands/Fun/Agreed.js
@@ -1,7 +1,13 @@
 /* eslint-disable linebreak-style */
 module.exports.run = async (Client, message, args, prefix) => {
   if (!message.content.startsWith(prefix)) return;
-  message.channel.send(`I agree with <@${message.author.id}>`);
+  let user = message.author;
+  if (message.mentions.users.first()) {
+    user = message.mentions.users.first();
+  } else if (Client.guilds.cache.get('781932135631028244').members.cache.get(args[0])) {
+    user = Client.guilds.cache.get('781932135631028244').members.cache.get(args[0]).user;
+  }
+  message.channel.send(`I agree with <@${user.id}>`);
   message.channel.send('https://tenor.com/view/true-true-true-i-agree-agreement-agreed-gif-23733447');
 };
 
