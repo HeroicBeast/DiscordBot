d545c838e131ce37f751ea18f45f771c1975991b
HeroicBeast <73134851+HeroicBeast@users.noreply.github.com>
2022-01-19 10:23:04 +0530

    Logs

--- mongo.js
+++ mongo.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 const { mongoPath } = require('./config.json');
+const { Client } = require('./index');
 
 module.exports = async () => {
   await mongoose.connect(mongoPath, {
@@ -14,4 +15,7 @@ module.exports = async () => {
 
 mongoose.connection.on('connected', () => {
   console.log('✅ Connected to Database');
+  Client.on('ready', () => [
+    Client.channels.cache.get('932941302163734539').send('\\✅ Connected to Database'),
+  ]);
 });
